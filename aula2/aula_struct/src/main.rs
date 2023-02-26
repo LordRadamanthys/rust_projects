b2b732564df5a97d0dc2b35cfe4b36753f8e516f
Mateus lima de matos <mateuslima565@gmail.com>
2023-02-25 22:10:15 -0300

    metodos das structs

--- aula2/aula_struct/src/main.rs
+++ aula2/aula_struct/src/main.rs
@@ -13,6 +13,10 @@ impl Conta {
     fn decimo_terceiro(self)->f32{
         return self.Saldo/2.0;
     }
+
+    fn sacar(&mut self,valor:f32){
+        self.Saldo = self.Saldo-valor
+    }
 }
 
 fn main() {
@@ -20,13 +24,17 @@ fn main() {
         Nome: String::from("teste"),
         Idade: 1
     };
-    let conta: Conta = Conta {
+    let mut conta: Conta = Conta {
         Titular: titular,
         Saldo: 200.0
     };
 
+    conta.sacar(50.0);
     println!("Nome {}", conta.Titular.Nome);
     println!("Idade {}", conta.Titular.Idade);
     println!("Saldo {}", conta.Saldo);
+    println!("Saldo com saque {}", conta.Saldo);
     println!("Decimo {}", conta.decimo_terceiro());
+    
+    
 }
